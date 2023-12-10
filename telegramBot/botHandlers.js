dad118024d284964ea431494fb74d0e241b5651c
Serhiy Ctrax <spoltoratsk@gmail.com>
2023-12-10 19:11:12 +0200

    Setup tg bot

--- telegramBot/botHandlers.js
+++ telegramBot/botHandlers.js
@@ -2,7 +2,7 @@ import User from "../models/userModel";
 
 
 
-export const handleStart = (bot, chatId) =>{
+export const handleStart = (bot, chatId) => {
     bot.sendMessage(chatId, 'Select an action:', {
         reply_markup: {
             inline_keyboard: [
@@ -13,29 +13,29 @@ export const handleStart = (bot, chatId) =>{
     });
 };
 
-export const handleCallbackQuery = (bot,chatId,data) =>{
-    switch (data){
+export const handleCallbackQuery = (bot, chatId, data) => {
+    switch (data) {
         case 'create':
-            bot.sendMessage(chatId,'Fill users fields', {
+            bot.sendMessage(chatId, 'Fill users fields', {
                 reply_markup: {
                     inline_keyboard: [
-                        [{text: "Username", callback_data: 'fill_username'}],
-                        [{text: "Email", callback_data: 'fill_email'}],
-                        [{text: "Password", callback_data: 'fill_password'}],
-                        [{text: "Role", callback_data: 'fill_role'}]    
+                        [{ text: "Username", callback_data: 'fill_username' }],
+                        [{ text: "Email", callback_data: 'fill_email' }],
+                        [{ text: "Password", callback_data: 'fill_password' }],
+                        [{ text: "Role", callback_data: 'fill_role' }]
                     ]
                 }
             });
             break;
         case 'login':
-            bot.sendMessage(chatId,'Fill fields', {
+            bot.sendMessage(chatId, 'Fill fields', {
                 reply_markup: {
                     inline_keyboard: [
-                        [{text: "Email", callback_data: 'login_email'}],
-                        [{text: "Password", callback_data: 'login_password'}]   
+                        [{ text: "Email", callback_data: 'login_email' }],
+                        [{ text: "Password", callback_data: 'login_password' }]
                     ]
                 }
-            });  
+            });
             break;
     };
 };
